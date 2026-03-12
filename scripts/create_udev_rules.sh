701b527295cf3b87a9d9818a72948b216300bbf7
N-rwal <104368706+N-rwal@users.noreply.github.com>
2026-03-12 09:54:59 +0100

    Refactor udev rules script to use script directory

--- scripts/create_udev_rules.sh
+++ scripts/create_udev_rules.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-echo "start copy rplidar.rules to  /etc/udev/rules.d/"
-colcon_cd sllidar_ros2
-sudo cp scripts/rplidar.rules  /etc/udev/rules.d
-echo " "
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo "start copy rplidar.rules to /etc/udev/rules.d/"
+sudo cp "$SCRIPT_DIR/rplidar.rules" /etc/udev/rules.d/
+
+echo ""
 echo "Restarting udev"
 echo ""
+
 sudo service udev reload
 sudo service udev restart
-echo "finish "
+
+echo "finish"
